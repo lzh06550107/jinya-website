5a2fe9bc704f9bcf264f4f9adb39141f2758e022
LiaoZhongHao <liao08022040@gmail.com>
2026-09-22 18:55:55 +0800

    revert(news): restore article editor before hidden-field cleanup

--- application/common/service/cms/render/NewsDetailRenderService.php
+++ application/common/service/cms/render/NewsDetailRenderService.php
@@ -64,6 +64,7 @@ class NewsDetailRenderService extends AbstractRenderService
             'latestArticles' => $latest,
             'latestColumns' => [array_slice($latest, 0, 4), array_slice($latest, 4, 4)],
             'metadata' => [
+                'author' => $article['author'],
                 'source' => $article['source'],
                 'publish_date' => $article['publish_date'],
                 'views' => $article['views'],
